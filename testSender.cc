bfcb18e1244409b313b1d99677020bd44c7b2962
nashfisch <nash.fisch88@gmail.com>
2023-10-31 16:24:18 -0600

    Changed test setup

--- testSender.cc
+++ testSender.cc
@@ -1,6 +1,7 @@
 #include "Sender.h"
 #include "gtest/gtest.h"
 
+/*
 class SenderTest : public testing::Test {
     protected:
         Sender* w_;
@@ -10,23 +11,36 @@ class SenderTest : public testing::Test {
     void TearDown() override {
         delete w_;
     }
-};
+};*/
 
 TEST_F(SenderTest, setSizeTest){
+    Sender* w_;
+    w_ = new Sender(0);
+
     w_->setWinSize(0);
     ASSERT_FALSE(w_->canAddNew());
+
+    delete w_;
 }
 
 TEST_F(SenderTest, addNewTest){
+    Sender* w_;
+    w_ = new Sender(0);
+
     w_->setWinSize(2);
     EXPECT_EQ(w_->addNew(), 0);
     EXPECT_EQ(w_->nrSeqInWin(), 1);
     EXPECT_EQ(w_->addNew(), 1);
     EXPECT_EQ(w_->addNew(), -1);
     EXPECT_EQ(w_->nrSeqInWin(), 2);
+
+    delete w_;
 }
 
 TEST_F(SenderTest, testAck){
+    Sender* w_;
+    w_ = new Sender(0);
+
     w_->setWinSize(2);
     EXPECT_EQ(w_->addNew(), 0);
     EXPECT_EQ(w_->nrSeqInWin(), 1);
@@ -34,4 +48,6 @@ TEST_F(SenderTest, testAck){
     EXPECT_EQ(w_->nrSeqInWin(), 2);
     w_->acknowledge(1);
     EXPECT_EQ(w_->nrSeqInWin(), 1);
+
+    delete w_;
 }
